a68627de71ee79b2aac2e11e37b3e589abeb6dd1
kumavis <aaron@kumavis.me>
2026-01-06 00:22:57 -1000

    wip: minimal test

--- tests/third_party_handoffs.py
+++ tests/third_party_handoffs.py
@@ -213,76 +213,76 @@ class HandoffRemoteAsExporter(HandoffTestCase):
             self.r2g_privkey.sign(handoff_receive.to_syrup())
         )
 
-    def test_valid_handoff(self):
-        """ Valid handoff receive, gift already deposited """
-        signed_handoff_give = self.make_valid_handoff()
-        handoff_give = signed_handoff_give.object
+    # def test_valid_handoff(self):
+    #     """ Valid handoff receive, gift already deposited """
+    #     signed_handoff_give = self.make_valid_handoff()
+    #     handoff_give = signed_handoff_give.object
 
-        # Deposit the gift with the exporter
-        deposit_gift_msg = captp_types.OpDeliverOnly(
-            self.g2e_session.bootstrap_object.to_desc_export(),
-            [Symbol("deposit-gift"), handoff_give.gift_id, self.g2e_greeter_refr]
-        )
-        self.g2e_session.send_message(deposit_gift_msg)
+    #     # Deposit the gift with the exporter
+    #     deposit_gift_msg = captp_types.OpDeliverOnly(
+    #         self.g2e_session.bootstrap_object.to_desc_export(),
+    #         [Symbol("deposit-gift"), handoff_give.gift_id, self.g2e_greeter_refr]
+    #     )
+    #     self.g2e_session.send_message(deposit_gift_msg)
 
-        # Withdraw the gift from the exporter
-        signed_handoff_receive = self.make_valid_handoff_receive(signed_handoff_give)
-        withdraw_gift_msg = captp_types.OpDeliver(
-            self.r2e_session.bootstrap_object.to_desc_export(),
-            [Symbol("withdraw-gift"), signed_handoff_receive],
-            False,
-            self.r2e_session.next_import_object
-        )
-        self.r2e_session.send_message(withdraw_gift_msg)
+    #     # Withdraw the gift from the exporter
+    #     signed_handoff_receive = self.make_valid_handoff_receive(signed_handoff_give)
+    #     withdraw_gift_msg = captp_types.OpDeliver(
+    #         self.r2e_session.bootstrap_object.to_desc_export(),
+    #         [Symbol("withdraw-gift"), signed_handoff_receive],
+    #         False,
+    #         self.r2e_session.next_import_object
+    #     )
+    #     self.r2e_session.send_message(withdraw_gift_msg)
 
-        resolved_handoff = self.r2e_session.expect_promise_resolution(
-            withdraw_gift_msg.exported_resolve_me_desc
-        )
-        self.assertEqual(resolved_handoff.args[0], Symbol("fulfill"))
-        self.assertIsInstance(resolved_handoff.args[1], captp_types.DescImportObject)
+    #     resolved_handoff = self.r2e_session.expect_promise_resolution(
+    #         withdraw_gift_msg.exported_resolve_me_desc
+    #     )
+    #     self.assertEqual(resolved_handoff.args[0], Symbol("fulfill"))
+    #     self.assertIsInstance(resolved_handoff.args[1], captp_types.DescImportObject)
 
-    def test_valid_handoff_wait_deposit_gift(self):
-        """ Valid handoff receive, sending deposite gift later """
-        signed_handoff_give = self.make_valid_handoff()
-        handoff_give = signed_handoff_give.object
+    # def test_valid_handoff_wait_deposit_gift(self):
+    #     """ Valid handoff receive, sending deposite gift later """
+    #     signed_handoff_give = self.make_valid_handoff()
+    #     handoff_give = signed_handoff_give.object
 
-        # Withdraw the gift from the exporter
-        signed_handoff_receive = self.make_valid_handoff_receive(signed_handoff_give)
-        withdraw_gift_msg = captp_types.OpDeliver(
-            self.r2e_session.bootstrap_object.to_desc_export(),
-            [Symbol("withdraw-gift"), signed_handoff_receive],
-            False,
-            self.r2e_session.next_import_object
-        )
-        self.r2e_session.send_message(withdraw_gift_msg)
+    #     # Withdraw the gift from the exporter
+    #     signed_handoff_receive = self.make_valid_handoff_receive(signed_handoff_give)
+    #     withdraw_gift_msg = captp_types.OpDeliver(
+    #         self.r2e_session.bootstrap_object.to_desc_export(),
+    #         [Symbol("withdraw-gift"), signed_handoff_receive],
+    #         False,
+    #         self.r2e_session.next_import_object
+    #     )
+    #     self.r2e_session.send_message(withdraw_gift_msg)
 
-        # Deposit the gift with the exporter
-        deposit_gift_msg = captp_types.OpDeliverOnly(
-            self.g2e_session.bootstrap_object.to_desc_export(),
-            [Symbol("deposit-gift"), handoff_give.gift_id, self.g2e_greeter_refr]
-        )
-        self.g2e_session.send_message(deposit_gift_msg)
+    #     # Deposit the gift with the exporter
+    #     deposit_gift_msg = captp_types.OpDeliverOnly(
+    #         self.g2e_session.bootstrap_object.to_desc_export(),
+    #         [Symbol("deposit-gift"), handoff_give.gift_id, self.g2e_greeter_refr]
+    #     )
+    #     self.g2e_session.send_message(deposit_gift_msg)
 
-        # Wait for a promise to the gifted object
-        initial_response = self.r2e_session.expect_message_to(withdraw_gift_msg.exported_resolve_me_desc)
-        self.assertEqual(initial_response.args[0], Symbol("fulfill"))
+    #     # Wait for a promise to the gifted object
+    #     initial_response = self.r2e_session.expect_message_to(withdraw_gift_msg.exported_resolve_me_desc)
+    #     self.assertEqual(initial_response.args[0], Symbol("fulfill"))
 
-        # Clients may return a promise, or the actual object
-        if isinstance(initial_response.args[1], captp_types.DescImportPromise):
-            # Send `op:listen` to get notified for the handoff vow resolution
-            listen_on_vow_msg = captp_types.OpListen(
-                initial_response.args[1].to_desc_export(),
-                self.r2e_session.next_import_object,
-                True
-            )
-            self.r2e_session.send_message(listen_on_vow_msg)
-            second_response = self.r2e_session.expect_promise_resolution(listen_on_vow_msg.exported_resolve_me_desc)
-            self.assertEqual(second_response.args[0], Symbol("fulfill"))
-            resolved_handoff = second_response.args[1]
-        else:
-            resolved_handoff = initial_response.args[1]
+    #     # Clients may return a promise, or the actual object
+    #     if isinstance(initial_response.args[1], captp_types.DescImportPromise):
+    #         # Send `op:listen` to get notified for the handoff vow resolution
+    #         listen_on_vow_msg = captp_types.OpListen(
+    #             initial_response.args[1].to_desc_export(),
+    #             self.r2e_session.next_import_object,
+    #             True
+    #         )
+    #         self.r2e_session.send_message(listen_on_vow_msg)
+    #         second_response = self.r2e_session.expect_promise_resolution(listen_on_vow_msg.exported_resolve_me_desc)
+    #         self.assertEqual(second_response.args[0], Symbol("fulfill"))
+    #         resolved_handoff = second_response.args[1]
+    #     else:
+    #         resolved_handoff = initial_response.args[1]
 
-        self.assertIsInstance(resolved_handoff, captp_types.DescImportObject)
+    #     self.assertIsInstance(resolved_handoff, captp_types.DescImportObject)
 
     def test_handoff_receive_invalid_handoff_count(self):
         """ Reject handoff-receive with invalid (already used) handoff count """
